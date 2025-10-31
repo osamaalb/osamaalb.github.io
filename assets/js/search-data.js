217dda6efc9c4697f47824188d9c4ae3a0559bc0
osamaalb <osama.alb96@gmail.com>
2025-10-31 08:03:26 +0000

    Deploying to gh-pages from @ osamaalb/osamaalb.github.io@615dc3e18d3aaec8bf5ca78e90124933bac00f33 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,14 +121,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6F%73%61%6D%61.%61%6C%6C%61%62%77%61%6E%69@%6C%69%6D%6F%73.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-hal',
+        title: 'HAL',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://cv.hal.science/osama-allabwani", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/osamaallabwani", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0000-1412-5857", "_blank");
         },
       },{
         id: 'social-rss',
